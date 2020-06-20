11dbe6e5f970a615136c514c2667691850010a31
bbseb <bbseb@Salon.home>
2020-06-20 19:06:15 +0200

    nfa035

---
+++ CNAM/src/nfa035/projet2/vue/Tableur.java
@@ -0,0 +1,14 @@
+package nfa035.projet2.vue;
+
+import javax.swing.SwingUtilities;
+
+public class Tableur {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SwingUtilities.invokeAndWait(Run {
+		}
+		);
+	}
+
+}
