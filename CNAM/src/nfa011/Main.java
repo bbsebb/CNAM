977d5e8227b957f9d527bbcf52da5de231b1f7af
bbseb <bbseb@Salon.home>
2020-11-04 21:40:54 +0100

    011

---
+++ CNAM/src/nfa011/Main.java
@@ -0,0 +1,29 @@
+package nfa011;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Main {
+
+	public static void main(String[] args)  {
+		boolean boucle = true;
+		String url = "jdbc:postgresql://192.168.1.41:5432/ComptesDB";
+		String login = "postgres";
+		String psw = "ga9399ghr";
+		try(Connection con = DriverManager.getConnection(url,login,psw)) {
+			DatabaseMetaData meta = con.getMetaData();
+			ResultSet rslt = meta.getTables(con.getCatalog(), null, "%",null);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		while(boucle) {
+			
+		}
+
+	}
+
+}
