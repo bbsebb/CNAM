d9db6c262fb60500ea8758d00091397a135de563
bbsebb <sebastien.burckhardt@gmail.com>
2020-11-06 22:40:05 +0100

    ee

--- CNAM/src/nfa011/Main.java
+++ CNAM/src/nfa011/Main.java
@@ -4,30 +4,22 @@ import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
+		// int value = (int) (Math.random() * 11);
+		//
+		// switch( value ) {
+		// case 0, 1, 2, 3, 4 -> System.out.println( "Petit chiffre" );
+		// case 5, 6, 7, 8, 9 -> System.out.println( "Grand chiffre" );
+		// default -> System.out.println( "Ce n'est plus un chiffre, mais un nombre" );
+		// }
 		ListeTables lt = new ListeTables();
-		int nbrTable = 0;
-		try {
-			nbrTable = lt.affichageTables();
-		} catch (SQLException e) {
-			System.out.println("Erreur dans l'affichage des tables : \n" + e.getMessage());
-		}
-		try (Scanner sc = new Scanner(System.in)) {
-			System.out.println("Selectionnez votre choix :");
-			int choix = sc.nextInt();
-			if (choix > 0 && choix < nbrTable + 1)
-				lt.affichageTable(choix - 1);
-		} catch (SQLException e) {
-			System.out.println("Erreur dans l'affichage de la table : \n" + e.getMessage());
-		}
+		int nbrTable = lt.affichageTables();
+		
 	}
 
 }
@@ -39,11 +31,12 @@ class ListeTables {
 	String login = "postgres";
 	String psw = "ga9399ghr";
 
-	void Listetable() throws SQLException {
+	void Listetable() {
 		try {
 			Class.forName("org.postgresql.Driver");
 		} catch (ClassNotFoundException e1) {
-			System.out.println("Erreur de connection à la BDD");
+			System.out.println("Erreur de connection ï¿½ la BDD");
+			;
 		}
 
 		try (Connection con = DriverManager.getConnection(url, login, psw)) {
@@ -54,15 +47,19 @@ class ListeTables {
 					tables.add(rslt.getString(3));
 				}
 			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
 
-	int affichageTables() throws SQLException {
+	int affichageTables() {
 		int i = 1;
 		try {
 			Class.forName("org.postgresql.Driver");
 		} catch (ClassNotFoundException e1) {
-			System.out.println("Erreur de connection à la BDD");
+			System.out.println("Erreur de connection ï¿½ la BDD");
+			;
 		}
 
 		try (Connection con = DriverManager.getConnection(url, login, psw)) {
@@ -75,44 +72,19 @@ class ListeTables {
 				while (rslt.next()) {
 					tables.add(rslt.getString(3));
 					System.out.println(i++ + " : " + rslt.getString(3));
+
 				}
-				System.out.println("0 : Quitter");
 			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 
 		return i;
 	}
 
-	void affichageTable(int numTable) throws SQLException,  IndexOutOfBoundsException{
-		String SQL = "SELECT * FROM " + tables.get(numTable);
-		if(numTable < 0 || numTable > tables.size() -1)
-			throw new IndexOutOfBoundsException();
-		try {
-			Class.forName("org.postgresql.Driver");
-		} catch (ClassNotFoundException e1) {
-			System.out.println("Erreur de connection à la BDD");
-		}
+	void affichageTable(int numTable) {
 
-		try (Connection con = DriverManager.getConnection(url, login, psw); Statement st = con.createStatement();) {
-
-			try (ResultSet rs = st.executeQuery(SQL)) {
-				ResultSetMetaData meta = rs.getMetaData();
-				for (int i = 1; i <= meta.getColumnCount(); i++) {
-					System.out.printf("%-17s", "|" + meta.getColumnName(i) + "|");
-				}
-				System.out.println();
-				while (rs.next()) {
-					for (int i = 1; i <= meta.getColumnCount(); i++) {
-						String str = rs.getObject(1).toString();
-						if (str.length() > 15)
-							str = str.substring(0, 14);
-						System.out.printf("%-17s", "|" + rs.getString(i) + "|");
-					}
-					System.out.println();
-
-				}
-			}
-		}
 	}
 
 }
