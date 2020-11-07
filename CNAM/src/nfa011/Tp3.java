bdf2e3db0f83856fc1fdeeec0cd08d54d40bf5df
bbseb <bbseb@Salon.home>
2020-11-07 21:01:05 +0100

    tp3

---
+++ CNAM/src/nfa011/Tp3.java
@@ -0,0 +1,15 @@
+package nfa011;
+
+import java.util.Scanner;
+
+public class Tp3 {
+	static Scanner sc = new Scanner(System.in);	
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Bienvenu");
+		System.out.println("Entrer l'url de la BDD :");
+		String url = sc.nextLine();
+	}
+
+}
