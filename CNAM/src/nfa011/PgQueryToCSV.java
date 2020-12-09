5779dc5d11d4d5ef9fb12ed7cc282714a756a365
bbsebb <sebastien.burckhardt@gmail.com>
2020-12-09 21:47:05 +0100

    tt

--- CNAM/src/nfa011/PgQueryToCSV.java
+++ CNAM/src/nfa011/PgQueryToCSV.java
@@ -3,56 +3,107 @@ package nfa011;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class PgQueryToCSV {
 
 	public static void main(String[] args) {
+		try {
+			Class.forName("org.postgresql.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
 
-	}
-
-	class MyConnection {
-		static final String DRIVER = "org.postgresql.Driver";
-		private String url;
-		private String login;
-		private String pw;
-
-		public MyConnection(String url, String login, String pw) {
-			this.url = url;
-			this.login = login;
-			this.pw = pw;
-
-			try {
-				Class.forName("org.postgresql.Driver");
-			} catch (ClassNotFoundException e) {
-				e.printStackTrace();
-			}
-
+		try (Scanner sc = new Scanner(System.in)) {
+			
+			System.out.println("Bienvenu");
+			System.out.println("Entrer l'url de la BDD :");
+			String url = sc.nextLine();
+			System.out.println("Entrer votre login :");
+			String login = sc.nextLine();
+			System.out.println("Entrer votre pw:");
+			String pw = sc.nextLine();
+			
 			try (Connection con = DriverManager.getConnection(url, login, pw)) {
 				System.out.println("Connection à la bdd reussi ...");
+				boolean menu = true;
+				MyQuery q = null;
+				while (menu) {
+					
+					afficherMenu();
+					
+					int chx = sc.nextInt();
+					sc.nextLine();
+					switch (chx) {
+						case 1 -> {
+							System.out.println("Entrer votre requete 'SELECT'");
+							String sql = sc.nextLine();
+							String rgx = "^SELECT[^;]*(;|.)$"; // La requête commence par SELECT et se termine par un ; ou rien mais il ne peux rien avoir après un ;
+							if(Pattern.compile(rgx, Pattern.CASE_INSENSITIVE).matcher(sql).matches()) // On vérifie que la requete est correcte
+								q = new MyQuery( con, sql);
+							else
+								System.err.println("Votre requète est incorrecte");
+						}
+						
+						case 3 -> {
+							menu = false;
+						}
+						default -> {
+							System.err.println("Entrer 1 ou 2 !");
+						}
+					}
+				}
+				
 			} catch (SQLException e) {
-				System.err.println("La connexion a echoué : \n" + e.getMessage());
+				System.err.println("La connexion a echoué : \n"+e.getMessage());
 			}
 		}
-
 	}
 
-	class MyQuery {
-		private Connection con;
-		private String requete;
-		
-		public MyQuery(Connection con, String requete) {
-			super();
-			this.con = con;
-			this.requete = requete;
-		}
-		
-		
-		
+	/**
+	 * Affiche le menu
+	 */
+	static void afficherMenu() {
+		System.out.println("Menu");
+		System.out.println("-------------------------------------------");
+		System.out.println("1 : Exécuter une requête de type SELECT.");
+		System.out.println("2 : Exporter la résultat de la requète en format CSV");
+		System.out.println("3 : Quitter le programme.");
+	}
+}
 
+
+class MyQuery {
+	private Connection con;
+	private String requete;
+	
+	public MyQuery(Connection con, String requete) {
+		setCon(con);
+		setRequete(requete);
 	}
 
-	class WriteCSV {
-		private String path;
+	public Connection getCon() {
+		return con;
 	}
 
+	private void setCon(Connection con) {
+		this.con = con;
+	}
+
+	public String getRequete() {
+		return requete;
+	}
+
+	private void setRequete(String requete) {
+		this.requete = requete;
+	}
+	
+	
+	
+
+}
+
+class WriteCSV {
+	private String path;
 }
