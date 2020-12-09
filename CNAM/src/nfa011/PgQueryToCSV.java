7ccf1a649d15a05afadf5e632870f9b450b49f1e
bbsebb <sebastien.burckhardt@gmail.com>
2020-12-09 15:06:09 +0100

    tt

---
+++ CNAM/src/nfa011/PgQueryToCSV.java
@@ -0,0 +1,58 @@
+package nfa011;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class PgQueryToCSV {
+
+	public static void main(String[] args) {
+
+	}
+
+	class MyConnection {
+		static final String DRIVER = "org.postgresql.Driver";
+		private String url;
+		private String login;
+		private String pw;
+
+		public MyConnection(String url, String login, String pw) {
+			this.url = url;
+			this.login = login;
+			this.pw = pw;
+
+			try {
+				Class.forName("org.postgresql.Driver");
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+
+			try (Connection con = DriverManager.getConnection(url, login, pw)) {
+				System.out.println("Connection à la bdd reussi ...");
+			} catch (SQLException e) {
+				System.err.println("La connexion a echoué : \n" + e.getMessage());
+			}
+		}
+
+	}
+
+	class MyQuery {
+		private Connection con;
+		private String requete;
+		
+		public MyQuery(Connection con, String requete) {
+			super();
+			this.con = con;
+			this.requete = requete;
+		}
+		
+		
+		
+
+	}
+
+	class WriteCSV {
+		private String path;
+	}
+
+}
