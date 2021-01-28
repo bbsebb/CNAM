6d13c91cabbcac1e1875849c44ac75adba6ddac7
bbsebb <sebastien.burckhardt@gmail.com>
2021-01-29 00:47:54 +0100

    tt

--- CNAM/src/nfa011/Examen.java
+++ CNAM/src/nfa011/Examen.java
@@ -16,7 +16,7 @@ public class Examen {
 
 	public static void main(String[] args) {
 
-		String url = "jdbc:postgresql://192.168.1.41:5432/test";
+		String url = "jdbc:postgresql://localhost:5432/test";
 		String user = "postgres";
 		String password = "ga9399ghr";
 
@@ -30,22 +30,37 @@ public class Examen {
 
 		try (Connection con = DriverManager.getConnection(url, user, password); Statement st = con.createStatement()) {
 			ResultSet rs = st.executeQuery(sql);
+			
 			ResultSetMetaData rsMetaData = rs.getMetaData();
-			for (int i = 0; i < (12 + 24 + 24 + 54 + 14 + 14); i++) {
-				System.out.print("-");
+			int nbrColonne = rsMetaData.getColumnCount();
+			String formatageStrTuples = "";
+			String formatageStrTitre = "";
+			String separteur = "";
+			for(int i = 1; i<= nbrColonne; i++) {
+				int tailleCol = (rsMetaData.getColumnDisplaySize(i)>rsMetaData.getColumnName(i).length())?rsMetaData.getColumnDisplaySize(i):rsMetaData.getColumnName(i).length();
+				formatageStrTitre += "| %-"+tailleCol+"s |";
+				System.out.println(rsMetaData.getColumnDisplaySize(i));
+				if(rsMetaData.getColumnTypeName(i)=="varchar")
+					formatageStrTuples += "| %-"+tailleCol+"s |";
+				else if(rsMetaData.getColumnTypeName(i)=="int4")
+					formatageStrTuples += "| %"+tailleCol+"d |";
+				
+				for (int j = 0; j < tailleCol + 4; j++) {
+					separteur += "-";
+				}
 			}
-			System.out.println();
-			System.out.printf("| %-8s || %-20s || %-20s || %-50s || %-10s || %-10s |\n", rsMetaData.getColumnName(1),
+			/*
+			System.out.println(formatageStrTuples);
+			
+			System.out.printf(formatageStrTitre+"\n", rsMetaData.getColumnName(1),
 					rsMetaData.getColumnName(2), rsMetaData.getColumnName(3), rsMetaData.getColumnName(4),
-					rsMetaData.getColumnName(5), rsMetaData.getColumnName(6));
-			for (int i = 0; i < (12 + 24 + 24 + 54 + 14 + 14); i++) {
-				System.out.print("-");
-			}
+					rsMetaData.getColumnName(5), rsMetaData.getColumnName(6), rsMetaData.getColumnName(7));
+
 			System.out.println();
 			while (rs.next()) {
-				System.out.printf("| %8d || %20s || %20s || %50s || %10d || %010d |\n", rs.getInt(1), rs.getString(2),
-						rs.getString(3), rs.getString(4), rs.getInt(5), rs.getInt(6));
-			}
+				System.out.printf(formatageStrTuples+"\n", rs.getInt(1), rs.getString(2),
+						rs.getString(3), rs.getString(4), rs.getInt(5), rs.getString(6), rs.getString(7));
+			}*/
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
